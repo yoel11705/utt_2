ab257ac8346ab8b2873ba5a74f578985cdd1e138
yoel11705 <138893653+yoel11705@users.noreply.github.com>
2023-08-11 09:39:49 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+  /** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports ={
+ ...nextConfig,
+      env: {
+     ZAPATITOS_API_KEY:"AIzaSyCWHRhKJj311HL887BddOdU2j2OQDprxmU",
+     ZAPATITOS_AUTH_DOMAIN:"a-ruth.firebaseapp.com",
+     ZAPATITOS_PROJECT_ID:"a-ruth",
+     ZAPATITOS_STORAGE_BUCKET:"a-ruth.appspot.com",
+     ZAPATITOS_MESSAGING_SENDER_ID:"1033472977558",
+     ZAPATITOS_APP_ID:"1:1033472977558:web:e76d0ed7aaeea63725b0b7"
+     },
+}
